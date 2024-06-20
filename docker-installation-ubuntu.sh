5ab047b863a5cbae438ac47c15a482a41271e883
itsmohit180 <159622319+itsmohit180@users.noreply.github.com>
2024-06-20 21:36:16 +0530

    Update docker-installation-ubuntu.sh

--- docker-installation-ubuntu.sh
+++ docker-installation-ubuntu.sh
@@ -15,4 +15,9 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 docker --version
